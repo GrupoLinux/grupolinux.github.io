6d395e7793533f61a9b9dfed25ba9ba766c28bce
Henry Jr <hmjsite@gmail.com>
2020-05-20 15:26:12 -0300

    alteracao geral
    Signed-off-by: Henry Jr <hmjsite@gmail.com>

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -2,4 +2,24 @@ $(document).ready(function () {
     $('[data-toggle="offcanvas"]').click(function () {
         $('.row-offcanvas').toggleClass('active')
     });
+
+    // grab an element
+
+    $("#header").headroom({
+        "offset": 205,
+        "tolerance": 5,
+        "classes": {
+            "initial": "animated",
+            "pinned": "flipInX",
+            "unpinned": "flipOutX"
+        }
+    });
+
+    $('[data-toggle=offcanvasleft]').click(function() {
+        $('.row-offcanvas-left').toggleClass('active');
+    });
+
+    $('[data-toggle=offcanvasright]').click(function() {
+        $('.row-offcanvas-right').toggleClass('active');
+    });
 });
